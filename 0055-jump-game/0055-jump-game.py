f29def2cd5ec8a69a1078f986b847fa81828e947
Gina <47096343+nnjh12@users.noreply.github.com>
2024-06-06 13:58:27 -0400

    Time: 6514 ms (5%), Space: 20.2 MB (5.05%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        mem = {}
+        return self.canJumpHelper(nums, 0, mem)
+  
+    def canJumpHelper(self, nums: List[int], start: int, mem: dict) -> bool:
+        # If already visited:
+        if mem.get(start) is not None:
+            #print("already visited")
+            return mem.get(start)
+
+        n = len(nums) - start
+        # Edge case
+        if n < 2:
+            #Wprint("case1-true")
+            mem[start] = True
+            return True
+
+        # Need to handle the case when nums[0] is 0.
+        if nums[start] == 0:
+            #print("case2-false")
+            mem[start] = False
+            return False
+        
+        # If possible max jumps is greater or equal to distance to the last index
+        if nums[start] >= n-1:
+            #print("case3-true")
+            mem[start] = True
+            return True
+
+        # We can jump 1 to nums[0] if nums[i] is not 0.
+        for i in range(1, nums[start] + 1):
+            if self.canJumpHelper(nums, start + i, mem):
+                mem[start] = True
+                break
+            else:
+                mem[start] = False
+        return mem[start]
