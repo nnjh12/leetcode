89b07a2a518da53022573703dabb8be71bd178d0
Gina <47096343+nnjh12@users.noreply.github.com>
2024-06-07 12:05:43 -0400

    Time: 129 ms (33.08%), Space: 17.7 MB (41.25%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,4 +1,27 @@
 class Solution:
+    # Attempt2. Linear
+    def jump(self, nums: List[int]) -> int:
+        goal = len(nums) - 1
+        jumps = 0
+        curMaxReach = 0 # The current max reach
+        nextMaxReach = 0 # The max reach after one more jump
+
+        for i in range(goal):
+            # Calculate possible farthest reach.
+            possibleReach = min(i + nums[i], goal)
+            nextMaxReach = max(nextMaxReach, possibleReach)
+
+            print(nextMaxReach)
+
+            # If we arrived at curMaxReach, jump to nextMaxReach.
+            if i == curMaxReach:
+                jumps += 1
+                curMaxReach = nextMaxReach
+
+        return jumps
+
+    """
+    # Attempt1. DP - not very fast
     def jump(self, nums: List[int]) -> int:
         mem = {}
         return self.jumpHelper(nums, 0, mem)
@@ -31,15 +54,8 @@ class Solution:
         # From the current position, we can jump from 1 to amount of energy.
         # Find the minimum jumps to arrive to the last index
         possibleJumps = [1 + self.jumpHelper(nums, start + i, mem) for i in range(1, energy + 1)]
-        
-        # for i in range(1, energy + 1):
-        #     jump = self.jumpHelper(nums, start + i, mem)
-        #     possibleJumps.append(jump)
-        
-        #[1 + self.jumpHelper(nums, start + i, mem) for i in range(1, energy + 1)]
-        
         minJump = min(possibleJumps)
         mem[start] = minJump
         return minJump
-
+    """
 
