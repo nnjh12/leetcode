2e69009f24eb192a7e66e24a4ece5d8b9093c6c2
Gina <47096343+nnjh12@users.noreply.github.com>
2024-06-11 00:36:44 -0400

    Time: 71 ms (6.01%), Space: 16.6 MB (15.71%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,30 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        n = len(s)
+        symbols = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+        values = [1, 5, 10, 50, 100, 500, 1000]
+        result = 0
+        
+        for i in range(1, n):
+            print(i)
+            s1 = s[i-1]
+            s2 = s[i]
+            if self.isInOrder(symbols, s1, s2):
+                result += values[symbols.index(s1)]
+            else:
+                result -= values[symbols.index(s1)]
+            print(result)
+        
+        result += values[symbols.index(s[n-1])]
+        return result
+
+
+    def isInOrder(self, symbols: list[str], s1: str, s2: str) -> bool:
+        distance = symbols.index(s1) - symbols.index(s2)
+        if distance == -1:
+            return False
+        elif distance >= 0:
+            return True
+        else:
+            print('Invalid Roman')
+
